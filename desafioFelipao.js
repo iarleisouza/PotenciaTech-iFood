429317a457c50a6dbdf0859a54b87dbe76ea3f3c
iarleisouza <65626531+iarleisouza@users.noreply.github.com>
2023-10-30 08:23:45 -0300

    Add files via upload

---
+++ desafioFelipao.js
@@ -0,0 +1,29 @@
+/* 
+Potência Tech iFood - Programação do Zero
+                Desafio Felipão 
+*/
+
+let nome = "Iarlei"
+let nivel = undefined
+
+let xp = Math.random() * 11000  
+
+
+if (xp < 1000) {
+    nivel = "Ferro"
+} else if (xp > 1000 && xp <= 2000){
+    nivel = "Bronze"
+} else if (xp > 2000 && xp <= 5000){
+    nivel = "Prata"
+} else if (xp > 6000 && xp <= 7000){
+    nivel = "Ouro"
+} else if (xp > 7000 && xp <= 8000){
+    nivel = "Bronze"
+} else if (xp > 8000 && xp <= 9000){
+    nivel= "Ascendente"
+} else if (xp > 9000 && xp <= 10000){
+    nivel = "Imortal"
+} else if (xp >= 10001){
+    nivel = "Radiante"
+}
+console.log(`O Herói de nome ${nome} está no nível ${nivel}`)
